e40518cfe13992d45f5464645d708a3574855aa9
Bernardo <berbeling@gmail.com>
2019-03-28 18:05:34 -0300

    Bugando nos 'pulos da struct'

--- main.c
+++ main.c
@@ -1,38 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//buffer-> [cont|i|scan|..........]
+//buffer-> [menu|i|contstruct|..........]
 
-struct pessoa{
-    char nome[30];
-    int idade,tel;
+struct pessoa
+{
+    char *nome;
+    int idade,matricula;
 };
 
-void apontaini(void* buffer,int* cont, int* i,int* scan){
-    cont=buffer;
-    i=cont+1;
-    scan=i+1;
-    printf("Entrouaponta\n");
+void* aponta(void** buffer, int** menu, int** i, int** contstruct)
+{
+    *menu=*buffer;
+    *i=*menu+1;
+    *contstruct=*i+1;
+}
+
+void adiciona(struct pessoa* p)
+{
+    //printf("Digite o nome:");
+    //scanf("%[^\n]s",p->nome);
+    //fgets(&p->nome,30,stdin);
+    printf("Digite a idade:");
+    scanf("%d",&p->idade);
+    printf("Digite a matricula:");
+    scanf("%d",&p->matricula);
 }
 
 void main()
 {
     void *buffer;
-    int *cont,*i,*num;
-    //struct pessoa *p;
+    int *menu, *i, *contstruct;
+    struct pessoa *p;
 
     buffer=malloc(sizeof(int)*3);
-    apontaini(buffer,cont,i,num);
-
-    printf("1.Adicionar Pessoa\n2.Remover\n3.Mostrar\n");
-    scanf("%d",num);
-
-    switch(*num)
+    aponta(&buffer,&menu,&i,&contstruct);
+    *contstruct=0;
+    do
     {
-        case 1:
-            printf("Entrou\n");
-            cont++;
-            buffer=realloc(buffer,sizeof(int)*3+(*cont*sizeof(struct pessoa)));
-            break;
-    }
+        printf("1.Adicionar Pessoa\n2.Remover\n3.Mostrar\n4.Sair\n"); 
+        scanf("%d",menu);
+    
+        switch(*menu)
+        {
+            case 1:
+                *contstruct=*contstruct+1;
+                buffer=realloc(buffer,(sizeof(int)*3+sizeof(struct pessoa)*(*contstruct)));
+                aponta(&buffer,&menu,&i,&contstruct);
+                p=contstruct+*contstruct;
+                adiciona(p);
+                printf("Registro adicionado!\n");
+                p=contstruct+1;
+                break;
+
+            case 2:
+                printf("Entrou no 2\n");
+                break;
+
+            case 3:
+                printf("Entrou no 3\n");
+                break;
+
+            default:
+                printf("Comando invalido!\n");
+                break;
+        }
+    } while(*menu!=4);
 }
